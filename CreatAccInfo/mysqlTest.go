fc914b9fb3d6b13db3d11133b0811cd291c4f170
dix <120189471@qq.com>
2018-03-21 22:22:49 +0800

    11

--- CreatAccInfo/mysqlTest.go
+++ CreatAccInfo/mysqlTest.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 "database/sql"
 	"fmt"
+
 )
 
 
@@ -38,30 +39,40 @@ type treadUserInfo struct{
 }
 
 func main() {
-	dblinkinfo:=DBlinkinfo{"mysql","root","3edcXSW@1qaz","127.0.0.1","3306","qmcounter"}
+	dblinkinfo := DBlinkinfo{"mysql", "root", "3edcXSW@1qaz", "127.0.0.1", "3306", "qmcounter"}
 	//db,err:=sql.Open("mysql","root:3edcXSW@1qaz@tcp(127.0.0.1:3306)/qmcounter")
-	db,err:=sql.Open(dblinkinfo.DBType,dblinkinfo.DBUser+":" +dblinkinfo.DBPWD+"@tcp("+dblinkinfo.DBIP+":"+dblinkinfo.DBPort+")/"+dblinkinfo.DBTableName)
+	db, err := sql.Open(dblinkinfo.DBType, dblinkinfo.DBUser+":"+dblinkinfo.DBPWD+"@tcp("+dblinkinfo.DBIP+":"+dblinkinfo.DBPort+")/"+dblinkinfo.DBTableName)
 	checkerr(err)
 	defer db.Close()
 	//dbins,err:=db.Prepare("INSERT mc_setting SET setting_key =test,setting_value=1.11")
 
 	//插入命令
 	//var treaduserinfo treadUserInfo
-	userseq,err:=db.Query("SELECT max(Seq) from mc_user")
+	userseq, err := db.Query("SELECT * from mc_user")
 	checkerr(err)
+	for userseq.Next() {
+
+	var treaduserinfo treadUserInfo
 	fmt.Println(userseq)
-	if userseq.Scan()==nil{
-		fmt.Print("userdeq=nil")
-	}else {
-		fmt.Print("判断错误")
+	userseq.Scan(&(treaduserinfo.seq))
+
+	fmt.Println(treaduserinfo.seq)
+	fmt.Println(treaduserinfo.userid)
 	}
+
+	//intreaduserinfo:= treadUserInfo{2,"000102","000102","888888",1,"11111",""}
+	//"+intreaduserinfo.seq+","+intreaduserinfo.userid+","+intreaduserinfo.name+","+intreaduserinfo.password+","+intreaduserinfo.isActive+","+intreaduserinfo.create_time+","+intreaduserinfo.lasstUpdata_time+")
+	ret,err:=db.Exec("INSERT into mc_user (seq,user_id,name,password,is_active,create_time,lastUpdate_time) VALUE (2,"2","2)")
+
+	checkerr(err)
+	fmt.Println(ret)
+
+}
 
 
 
 //	treaduserinfo:=treadUserInfo{}
 //	stmt err:=db.Prepare("INSERT ")
-
-}
 
 
 
